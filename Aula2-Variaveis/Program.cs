ec68c673f0551fb5c99618a86f22b46e44785dff
RAFAEL DE CARVALHO E SILVA VOLPE ALONSO DE SOUSA <rafael.csvasousa@senacsp.edu.br>
2025-11-03 20:42:20 -0300

    primeiro commit com codigo

---
+++ Aula2-Variaveis/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// f5 -> EXECUTAR EM MODO DEBUG
+// ctrl k + c -> COMENTA LINHA
+// ctrl k + u -> DESCOMENTA LINHA
+// crtl + shift + b -> COMPILAR
+
+// DECLARACAO DE UMA VARIAVEL
+string nome;
+
+// ATRIBUICAO DE VALOR A UMA VARIAVEL
+nome = "rafael sousa";
+
+Console.WriteLine(nome);
+
+nome = "joão";
+
+Console.WriteLine(nome);
+
+// DECLARACAO + ATRIBUICAO DE UMA CONSTANTE
+const string constante = "minha constante";
+
+Console.WriteLine(constante);
+
+// tipo nome = valor;
+int idade = 34;
+long inteiroGrande = 0;
+float alturaFloat = 1.8f;
+double altura = 1.8;
+char letra = 'A';
+
+bool boleano = true;
---
+++ Aula3-Operadores/Program.cs
@@ -0,0 +1,83 @@
+﻿//double x = 1;
+//int y = 1;
+//int z = 2;
+
+//double resultado = ((x + y) * z) / 3;
+
+//resultado += 100;
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Olá, Mundo!");
+//Console.WriteLine(resultado < 100);
+
+//Console.WriteLine("\n-----\n");
+
+//string nome = "Rafael";
+//string sobrenome = "Sousa";
+//Console.WriteLine(nome + sobrenome);
+
+//char letraA = 'Á';
+//char letraB = '1';
+//Console.WriteLine(letraA + 0);
+//Console.WriteLine(letraB + 0);
+
+//Console.WriteLine("\n-----\n");
+
+//Console.WriteLine("Escreva seu nome.");
+//string nomeUsuario = Console.ReadLine();
+//Console.WriteLine("O seu nome é: " + nomeUsuario);
+
+//Console.WriteLine("Em qual ano vc nasceu?");
+//string anoNascimentoUsuarioString = Console.ReadLine();
+//int idadeUsuario = 2025 - int.Parse(anoNascimentoUsuarioString);
+//Console.WriteLine("A sua idade é: " + idadeUsuario);
+
+//Peça para o usuário inserir dois números inteiros e mostre-os no Console.
+//int n1;
+//int n2;
+//Console.WriteLine("Informe um numero inteiro:");
+//n1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Informe outro numero inteiro:");
+//n2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Os numeros informados sao: " + n1 + " e " + n2 + ".");
+
+//Peça para o usuário inserir dois números inteiros,
+//mostre a soma dos números informados no Console.
+//string n1, n2;
+
+//Console.WriteLine("Informe um numero inteiro:");
+//n1 = Console.ReadLine();
+//Console.WriteLine("Informe outro numero inteiro:");
+//n2 = Console.ReadLine();
+
+//int n1int = int.Parse(n1);
+//int n2int = int.Parse(n2);
+//int soma = n1int + n2int;
+
+//Console.WriteLine("O resultado da soma eh: " + soma);
+
+//Peça para o usuário inserir dois números inteiros,
+//mostre a subtração dos números informados no Console.
+//Console.WriteLine("Informe um numero inteiro:");
+//int n1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Informe outro numero inteiro:");
+//int n2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("O valor da subtração eh: " + (n1 - n2));
+
+//Peça para o usuário inserir dois números inteiros,
+//mostre a multiplicação dos números informados no Console.
+//Console.WriteLine("Informe um numero inteiro:");
+//int n = int.Parse(Console.ReadLine());
+//Console.WriteLine("Informe outro numero inteiro:");
+//n *= int.Parse(Console.ReadLine());
+//Console.WriteLine("O valor da multiplicacao eh: " + n);
+
+//Peça para o usuário inserir dois números inteiros, mostre a divisão
+//dos números informados no Console (Cuidado com o tipo na hora de mostrar o resultado).
+//Console.WriteLine("Informe um numero inteiro:");
+//double n1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Informe outro numero inteiro:");
+//double n2 = double.Parse(Console.ReadLine());
+//double divisao = n1 / n2;
+//Console.WriteLine("O resultado da divisao eh: " + divisao);
+
