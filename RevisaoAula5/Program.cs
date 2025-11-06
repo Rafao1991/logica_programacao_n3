08557b0d3f3bda7ce7d29a709fc92728052cd556
RAFAEL DE CARVALHO E SILVA VOLPE ALONSO DE SOUSA <rafael.csvasousa@senacsp.edu.br>
2025-11-05 21:58:29 -0300

    aula 5

---
+++ RevisaoAula5/Program.cs
@@ -0,0 +1,25 @@
+﻿//Peça para o usuário inserir dois números inteiros e mostre-os no Console.
+
+int n1;
+int n2;
+
+// Escrevi uma mensagem no Console
+Console.WriteLine("Insira um numero inteiro: ");
+// Li o conteudo de uma linha, quando o usuario apertou a tecla ENTER
+// O console só aceita texto (string)
+// Logo é preciso converter a string para int
+n1 = int.Parse(Console.ReadLine());
+
+// Escrevi outra mensagem no Console
+Console.WriteLine("Insira mais um numero inteiro: ");
+// Novamente li o conteudo da linha, quando o usuario apertou a tecla ENTER
+n2 = int.Parse(Console.ReadLine());
+
+// Mostrar os dois numeros no Console
+Console.WriteLine("Voce digitou " + n1 + " e " + n2);
+
+int soma = n1 + n2;
+int sub = n1 - n2;
+
+Console.WriteLine("A soma eh: " + soma);
+Console.WriteLine("A subtracao eh: " + sub);
