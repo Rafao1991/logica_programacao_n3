cee7b00acd28a47c5acc431c22bb56c398d2af5e
RAFAEL DE CARVALHO E SILVA VOLPE ALONSO DE SOUSA <rafael.csvasousa@senacsp.edu.br>
2025-11-26 21:28:25 -0300

    Aula 11

---
+++ Aula11-FiltroPares/Program.cs
@@ -0,0 +1,17 @@
+﻿Random random = new Random();
+//List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+List<int> numeros = new();
+
+for (int i = 0; i < random.Next(1, 10001); i++)
+{
+    numeros.Add(random.Next(1, 101));
+}
+
+for (int i = 0; i < numeros.Count; i++)
+{
+    int numero = numeros[i];
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine(numero);
+    }
+}
---
+++ Aula11-Listas/Program.cs
@@ -0,0 +1,28 @@
+﻿// Notas
+// duas provas
+// media
+
+//int[] notas = new int[2];
+
+//Console.WriteLine("Nota p1:");
+//notas[0] = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Nota p2:");
+//notas[1] = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Nota final: " + ((notas[0] + notas[1]) / 2));
+
+List<int> notas = new List<int>();
+
+Console.WriteLine("Nota p1:");
+notas.Add(int.Parse(Console.ReadLine()));
+
+Console.WriteLine("Nota p2:");
+notas.Add(int.Parse(Console.ReadLine()));
+
+Console.WriteLine("Nota final: " + ((notas[0] + notas[1]) / 2));
+
+for (int i = 0; i < notas.Count; i++)
+{
+    Console.WriteLine(notas[i]);
+}
